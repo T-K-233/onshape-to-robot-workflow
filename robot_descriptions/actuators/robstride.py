6f3cfb3973af837588667c1a166b111f286c59b7
-T.K.- <tk233.xyz@gmail.com>
2026-05-03 20:41:51 -0700

    add: new sysid result from mujoco toolbox

--- robot_descriptions/actuators/robstride.py
+++ robot_descriptions/actuators/robstride.py
@@ -3,7 +3,7 @@
 ROBSTRIDE_00_ACTUATOR_PARAMS = {
     "velocity_limit"    : 32.987,   # rad/s
     "effort_limit"      : 14.0,     # Nm
-    "armature"          : 0.009,    # kgm^2
+    "armature"          : 0.013,    # kgm^2
 }
 """ Parameters for the Robstride 00 actuator. """
 
@@ -12,7 +12,7 @@ ROBSTRIDE_00_ACTUATOR_PARAMS = {
 ROBSTRIDE_01_ACTUATOR_PARAMS = {
     "velocity_limit"    : 50.13,    # rad/s
     "effort_limit"      : 17.0,     # Nm
-    "armature"          : 0.009,   # kgm^2
+    "armature"          : 0.013,   # kgm^2
 }
 """ Parameters for the Robstride 01 actuator. """
 
@@ -21,7 +21,7 @@ ROBSTRIDE_01_ACTUATOR_PARAMS = {
 ROBSTRIDE_02_ACTUATOR_PARAMS = {
     "velocity_limit"    : 65.25,    # rad/s
     "effort_limit"      : 17.0,     # Nm
-    "armature"          : 0.009,   # kgm^2
+    "armature"          : 0.013,   # kgm^2
 }
 """ Parameters for the Robstride 02 actuator. """
 
@@ -48,7 +48,7 @@ ROBSTRIDE_04_ACTUATOR_PARAMS = {
 ROBSTRIDE_05_ACTUATOR_PARAMS = {
     "velocity_limit"    : 76.39,    # rad/s
     "effort_limit"      : 5.5,      # Nm
-    "armature"          : 0.005,    # kgm^2
+    "armature"          : 0.006,    # kgm^2
 }
 """ Parameters for the Robstride 05 actuator. """
 
@@ -57,6 +57,6 @@ ROBSTRIDE_05_ACTUATOR_PARAMS = {
 ROBSTRIDE_06_ACTUATOR_PARAMS = {
     "velocity_limit"    : 76.39,    # rad/s
     "effort_limit"      : 36.0,     # Nm
-    "armature"          : 0.015,    # kgm^2
+    "armature"          : 0.016,    # kgm^2
 }
 """ Parameters for the Robstride 06 actuator. """
